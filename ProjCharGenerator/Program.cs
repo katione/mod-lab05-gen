3a8a271c37fce31e9006d68a77f45c3530e6f31b
katione0 <esimchikkatrine@gmail.com>
2024-04-17 00:36:41 +0300

    feat: add solution for tasks, add tests

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,23 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace generator
 {
-    class CharGenerator 
+    public class CharGenerator
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
+        public string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
         private char[] data;
         private int size;
         private Random random = new Random();
-        public CharGenerator() 
+        public CharGenerator()
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            size = syms.Length;
+            data = syms.ToCharArray();
         }
-        public char getSym() 
+        public char getSym()
         {
-           return data[random.Next(0, size)]; 
+            return data[random.Next(0, size)];
         }
+    }
+    public class Generator1
+    {
+        public string[] combinations;
+        public int[] Summ;
+        private int size;
+        private Random rand = new Random();
+        public Generator1() { int size = 0; }
+        public Generator1(string[] combinations, int[] values)
+        {
+            this.combinations = combinations;
+            Summ = new int[combinations.Length];
+            Summ[0] = values[0];
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                Summ[i] = Summ[i - 1] + values[i];
+            }
+
+        }
+
+        public void read_file(string name)
+        {
+            string[] str = File.ReadAllLines(name);
+            combinations = new string[str.Length];
+            Summ = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                string[] strr = str[i].Split(' ');
+                combinations[i] = strr[0];
+                size += int.Parse(strr[1]);
+                Summ[i] = size;
+            }
+        }
+        public string getSymm()
+        {
+            int sym = 0;
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                if (rand.Next(0, size) < Summ[i])
+                {
+                    sym = i;
+                    break;
+                }
+            }
+            return combinations[sym];
+        }
+
+    }
+    public class Generator2
+    {
+        private string[] combinations;
+        private double[] Summ;
+        private double size = 0;
+        private Random rand = new Random();
+        public Generator2() { int size = 0; }
+        public Generator2(string[] combinations, double[] values)
+        {
+            this.combinations = combinations;
+            Summ = new double[combinations.Length];
+            Summ[0] = values[0];
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                Summ[i] = Summ[i - 1] + values[i];
+            }
+
+        }
+
+        public void read_file(string name)
+        {
+            string[] str = File.ReadAllLines(name);
+            combinations = new string[str.Length];
+            Summ = new double[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                string[] strr = str[i].Split(' ');
+                combinations[i] = strr[0];
+                size += double.Parse(strr[1]);
+                Summ[i] = size;
+            }
+        }
+        public string getSymm()
+        {
+            int sym = 0;
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                if (rand.Next(0, Convert.ToInt32(size)) < Summ[i])
+                {
+                    sym = i;
+                    break;
+                }
+            }
+            return combinations[sym];
+        }
+
     }
     class Program
     {
@@ -25,21 +120,37 @@ namespace generator
         {
             CharGenerator gen = new CharGenerator();
             SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            for (int i = 0; i < 1000; i++)
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                char ch = gen.getSym();
+                if (stat.ContainsKey(ch))
+                    stat[ch]++;
+                else
+                    stat.Add(ch, 1); Console.Write(ch);
             }
             Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            foreach (KeyValuePair<char, int> entry in stat)
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                Console.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
             }
-            
+
+            Generator1 gen1 = new Generator1();
+            Generator2 gen2 = new Generator2();
+            string gen_res1 = " ";
+            gen1.read_file("file1.txt");
+            for (int i = 0; i < 1000; i++)
+            {
+                gen_res1 += gen1.getSymm();
+            }
+            File.WriteAllText("gen-1.txt", gen_res1);
+            string gen_res2 = " ";
+            gen2.read_file("file2.txt");
+            for (int j = 0; j < 1000; j++)
+            {
+                gen_res2 += gen2.getSymm();
+                gen_res2 += " ";
+            }
+            File.WriteAllText("gen-2.txt", gen_res2);
         }
     }
 }
-
---
+++ TestProject_laba5/UnitTest1.cs
@@ -0,0 +1,64 @@
+using generator;
+namespace TestProject_laba5;
+
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestGetSym()
+    {
+        CharGenerator gen = new CharGenerator();
+        char sym = gen.getSym();
+        Assert.IsTrue(gen.syms.Contains(sym.ToString()));
+    }
+    [TestMethod]
+    public void TestGenerator1()
+    {
+        Generator1 gen1 = new Generator1();
+        gen1.read_file("file1.txt");
+
+        for (int i = 0; i < 1000; i++)
+        {
+            string sym = gen1.getSymm();
+            Assert.IsNotNull(sym);
+        }
+    }
+    [TestMethod]
+    public void TestGenerator2()
+    {
+        Generator2 gen2 = new Generator2();
+        gen2.read_file("file2.txt");
+
+        for (int i = 0; i < 1000; i++)
+        {
+            string sym = gen2.getSymm();
+            Assert.IsNotNull(sym);
+        }
+    }
+    [TestMethod]
+    public void TestMetod4()
+    {
+        Generator1 gen = new Generator1();
+        gen.read_file("test1.txt");
+        var res = gen.getSymm();
+        Assert.IsTrue(res == "aa");
+    }
+    [TestMethod]
+    public void TestMetod5()
+    {
+        Generator1 gen = new Generator1();
+        gen.read_file("test1.txt");
+        var res = gen.getSymm();
+        Assert.AreEqual("aa", res);
+    }
+    [TestMethod]
+    public void TestMetod6()
+    {
+        Generator2 gen = new Generator2();
+        gen.read_file("test2.txt");
+        var res = gen.getSymm();
+        Assert.IsTrue(res == "aaa" || res == "bbb");
+    }
+}
+
